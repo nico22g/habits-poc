1c0134c6a8d80f3314bef2634316cf62701ca1ae
nico22g <nico22utn@gmail.com>
2025-09-09 18:36:06 -0400

    feat: Adding basic footer tabs structure by using Expo Router

---
+++ app/(tabs)/estadistics.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function EstadisticsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Estadistics</Text>
+    </View>
+  );
+}
